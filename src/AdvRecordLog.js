7fce3c1db481e3c767f71e4d94d996b797cab0f7
FedoraMark <fedora.mark@icloud.com>
2020-08-12 00:57:32 -0400

    begin react project

---
+++ src/AdvRecordLog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './AdvRecordLog.scss';
+
+function AdvRecordLog() {
+  return (
+    <div className="Log">
+      Hello, world!
+    </div>
+  );
+}
+
+export default AdvRecordLog;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AdvRecordLog from './AdvRecordLog';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AdvRecordLog />
   </React.StrictMode>,
   document.getElementById('root')
 );
