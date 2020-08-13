787961ce3d7e2ec4c9c507568f7c3b61c7bd32a8
FedoraMark <fedora.mark@icloud.com>
2020-08-13 00:01:12 -0400

    layout updates

--- src/GameLog.js
+++ src/GameLog.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from "classnames";
 import Collapse from "react-bootstrap/collapse";
+import Container from "react-bootstrap/Container";
+
 
 import './GameLog.scss';
 
@@ -34,30 +36,78 @@ class GameLog extends React.Component {
 	}
 
 	render_gameInfo = (event, date, dmObj) => {
+		var dmStr = '';
+		if ("name" in dmObj) {
+			dmStr = dmObj.name;
+			if ("dci" in dmObj) {
+				dmStr = dmStr + " (" + dmObj.dci + ")";
+			}
+		} else if ("dci" in dmObj) {
+			dmStr = dmObj.dci;
+		} 
+
 		return(
-			<ul className="infoWrapper">
-				{date !== '' && <li><h1 className="date">Date:</h1><p>{date}</p></li>}
-				{event !== '' && <li><h1 className="event">Event:</h1><p>{event}</p></li>}
-				{dmObj !== '' && <li><h1 className="dm">Dungeon Master:</h1><p>{"name" in dmObj && dmObj.name}{"dci" in dmObj && " (" + dmObj.dci + ")"}</p></li>}
-			</ul>
+			<Container>
+				<ul className="infoWrapper">
+					{date !== '' && <li><h1 className="date">Date:</h1><p>{date}</p></li>}
+					{event !== '' && <li><h1 className="event">Event:</h1><p>{event}</p></li>}
+					{dmStr !== '' && <li><h1 className="dm">Dungeon Master:</h1><p>{dmStr}</p></li>}
+				</ul>
+			</Container>
 		);
 	}
 
 	render_advNotes = (notesObj) => {
 		return (
-			<div className="notesWrapper wrapper">
-				<h1>Adventure Notes</h1>
-				<div className="box">
-					<p className="gameNotes bookFont">{notesObj.game}</p>
-					{"player" in notesObj && <hr />}
-					{"player" in notesObj && <p className="playerNotes bookFont">{notesObj.player}</p>}
-				</div>
-			</div>
+			<Container className="notesWrapper wrapper">
+					<h1>Adventure Notes</h1>
+					<div className="box">
+						<p className="gameNotes bookFont">{notesObj.game}</p>
+						{"player" in notesObj && <hr />}
+						{"player" in notesObj && <p className="playerNotes bookFont">{notesObj.player}</p>}
+					</div>
+			</Container>
 		)
 	}
 
 	render_advancement = (advObj) => {
+		return(
+			<Container className="advWrapper wrapper">
+					<h1>Advancement</h1>
+					<div className="box">
+					</div>
+			</Container>
+		);
+	}
 
+	render_rewards = (rewardObj) => {
+		return(
+			<Container className="rewardsWrapper wrapper">
+					<h1>Rewards</h1>
+					<div className="box">
+					</div>
+			</Container>
+		);
+	}
+
+	render_wealth = (wealthObj) => {
+		return(
+			<Container className="wealthWrapper wrapper">
+					<h1>Character Wealth</h1>
+					<div className="box">
+					</div>
+			</Container>
+		);
+	}
+
+	render_legacy = (legacyObj) => {
+		return(
+			<Container className="legacyContainer" className="legacyWrapper wrapper">
+					<h1>Legacy Events</h1>
+					<div className="box">
+					</div>
+			</Container>
+		);
 	}
 
 	render() {
@@ -71,9 +121,21 @@ class GameLog extends React.Component {
 					<div className="content">
 						{this.render_gameInfo(data.event,data.date,data.dungeonMaster)}
 						{this.render_advNotes(data.notes)}
-						{this.render_advancement(data.advancement)}
+
+						<div className="twoCol">
+							<div className="leftCol arCol">
+								{this.render_advancement(data.advancement)}
+								{this.render_rewards(data.rewards)}
+								{this.render_wealth(data.gameWealth)}
+							</div>
+
+							<div className="rightCol arCol">
+								{this.render_legacy(data.legacy)}
+							</div>
+						</div>
 					</div>
 				</Collapse>
+
 	    	</div>
 	  	)
 	}
