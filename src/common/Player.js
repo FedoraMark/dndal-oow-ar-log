329450c146d23d35604f6021564ed4fad6f25a5a
FedoraMark <fedora.mark@icloud.com>
2020-08-16 14:08:33 -0400

    info area fixes git status

--- src/common/Player.js
+++ src/common/Player.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
 import _map from 'lodash/map';
 
 import Wealth from "./Wealth.js";
@@ -56,59 +57,65 @@ export class Player extends Component {
     render() {
         return (
             <Container fluid className="playerBox">
-				<div className="infoItem">
-					<h1>Character Name:</h1>
-					<p>{this.state.character}</p>
-				</div>
-
-				{(this.state.player !== null || this.state.dci !== null) &&
+				<Row>
 					<div className="infoItem">
-						<h1>Player {this.state.player !== null ? "Name" : "DCI"}:</h1>
-						<p>{this.getPlayerDciStr()}</p>
+						<h1>Character Name:</h1>
+						<p>{this.state.character}</p>
 					</div>
-				}
 
-				{this.state.classes !== null &&
-					<div className="infoItem">
-						<h1>Classes & Levels:</h1>
-						<p>
-							{_map(this.state.classes, (level, clss) => {
-								return(
-									<span className="class" key={clss}>{clss + " (" + level + ")"}<span className="comma">, </span></span>
-								);
-							})}
-						</p>
-					</div>
-				}
+					{(this.state.player !== null || this.state.dci !== null) &&
+						<div className="infoItem">
+							<h1>Player {this.state.player !== null ? "Name" : "DCI"}:</h1>
+							<p>{this.getPlayerDciStr()}</p>
+						</div>
+					}
+				</Row>
 
-				{this.state.tier !== null &&
-					<div className="infoItem">
-						<h1>Tier:</h1>
-						<ul className="tierList">
-							<li className={this.state.tier > 0 ? "filled": ''}>1</li>
-							<li className={this.state.tier > 1 ? "filled": ''}>2</li>
-							<li className={this.state.tier > 2 ? "filled": ''}>3</li>
-							<li className={this.state.tier > 3 ? "filled": ''}>4</li>
-						</ul>
-						{/* <p>{this.state.tier}</p> */}
-					</div>
-				}
+				<Row>
+					{this.state.classes !== null &&
+						<div className="infoItem">
+							<h1>Classes & Levels:</h1>
+							<p>
+								{_map(this.state.classes, (level, clss) => {
+									return(
+										<span className="class" key={clss}>{clss + " (" + level + ")"}<span className="comma">, </span></span>
+									);
+								})}
+							</p>
+						</div>
+					}
 
-				{this.state.base !== null &&
-					<div className="infoItem">
-						<h1>Assigned Base:</h1>
-						<p>{this.state.base}</p>
-					</div>
-				}
+					{this.state.tier !== null &&
+						<div className="infoItem tierItem">
+							<h1>Tier:</h1>
+							<ul className="tierList">
+								<li className={this.state.tier > 0 ? "filled": ''}>1</li>
+								<li className={this.state.tier > 1 ? "filled": ''}>2</li>
+								<li className={this.state.tier > 2 ? "filled": ''}>3</li>
+								<li className={this.state.tier > 3 ? "filled": ''}>4</li>
+							</ul>
+							{/* <p>{this.state.tier}</p> */}
+						</div>
+					}
+				</Row>
 
-				{this.state.wealth !== null &&
-					<div className="infoItem">
-						<h1>Current Wealth:</h1>
-						<p>
-							<Wealth wealthObj={this.state.wealth} />
-						</p>
-					</div>
-				}
+				<Row>
+					{this.state.base !== null &&
+						<div className="infoItem">
+							<h1>Assigned Base:</h1>
+							<p>{this.state.base}</p>
+						</div>
+					}
+
+					{this.state.wealth !== null &&
+						<div className="infoItem">
+							<h1>Current Wealth:</h1>
+							<p>
+								<Wealth wealthObj={this.state.wealth} />
+							</p>
+						</div>
+					}
+				</Row>
 			</Container>
         );
     }
