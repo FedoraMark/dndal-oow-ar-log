8479019e00d3b8669f13adf472e2c764063494df
FedoraMark <fedora.mark@icloud.com>
2020-09-06 04:18:53 -0400

    roueter

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { HashRouter, Route } from "react-router-dom";
+import AdvRecordLog from "AdvRecordLog";
+import { ToastProvider } from "react-toast-notifications";
+
+class App extends Component {
+    render() {
+        return (
+            <HashRouter basename="/">
+				<Route path="/" component={Log} exact />
+				<Route path="/log" component={Log} />
+			</HashRouter>
+        );
+    }
+}
+
+const Log = () => <ToastProvider autoDismiss autoDismissTimeout="3000"><AdvRecordLog /></ToastProvider>
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import { HashRouter, Route } from "react-router-dom";
-import { ToastProvider } from "react-toast-notifications";
-import AdvRecordLog from "AdvRecordLog";
+
+import App from "App";
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <React.StrictMode>
-		<HashRouter basename={'/log'}>
-		  <Route path="/" component={ Log } exact />
-		</HashRouter>
+		<App />
 	</React.StrictMode>,
     document.getElementById("root")
 );
-
-// function Home() {} // TO BE DONE
-
-function Log() {
-    return (
-        <ToastProvider autoDismiss autoDismissTimeout="3000">
-			<AdvRecordLog />
-		</ToastProvider>
-    );
-}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
