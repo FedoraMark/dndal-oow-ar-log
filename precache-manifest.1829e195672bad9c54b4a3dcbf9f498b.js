6d2b9297908f81c878b0db65c71af7be98e50f52
FedoraMark <fedora.mark@icloud.com>
2020-09-06 03:30:52 -0400

    Updates

---
+++ precache-manifest.1829e195672bad9c54b4a3dcbf9f498b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5886c8d2ffe5dfa4b6d86efa541f8e",
+    "url": "/log/index.html"
+  },
+  {
+    "revision": "34786ecd478f559257bc",
+    "url": "/log/static/css/2.14d98803.chunk.css"
+  },
+  {
+    "revision": "38e85953bd438c56a34d",
+    "url": "/log/static/css/main.eb64f17b.chunk.css"
+  },
+  {
+    "revision": "34786ecd478f559257bc",
+    "url": "/log/static/js/2.16dfbb29.chunk.js"
+  },
+  {
+    "revision": "5e6dbcf372b2911b30bbaeb29c9d0b8c",
+    "url": "/log/static/js/2.16dfbb29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e85953bd438c56a34d",
+    "url": "/log/static/js/main.a2f72324.chunk.js"
+  },
+  {
+    "revision": "ba9c974a057edf0429b9",
+    "url": "/log/static/js/runtime-main.806f013f.js"
+  },
+  {
+    "revision": "11162204ef76d27e6b55c42f1425df9f",
+    "url": "/log/static/media/Bookinsanity-Bold.11162204.otf"
+  },
+  {
+    "revision": "87aea8858af5bf9a6aede0b768f0d1d5",
+    "url": "/log/static/media/Bookinsanity-BoldItalic.87aea885.otf"
+  },
+  {
+    "revision": "090a9fd37ee20ff75044f92c63784402",
+    "url": "/log/static/media/Bookinsanity-Italic.090a9fd3.otf"
+  },
+  {
+    "revision": "4b68d135c46a942455fcb132ae360815",
+    "url": "/log/static/media/Bookinsanity.4b68d135.otf"
+  },
+  {
+    "revision": "e73a294312bd965876bc8fc9944fd18a",
+    "url": "/log/static/media/Nodesto-Caps-Condensed-Italic.e73a2943.otf"
+  },
+  {
+    "revision": "e3f3c6957440986c5f03cf9bf57b27cf",
+    "url": "/log/static/media/Nodesto-Caps-Condensed.e3f3c695.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d0bfc202a5216918d0b3c1f182be0b87.js"
+  "/log/precache-manifest.1829e195672bad9c54b4a3dcbf9f498b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/log/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
