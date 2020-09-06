b66ccacff02354ccc532b02cc3c7a9a19f79d810
FedoraMark <fedora.mark@icloud.com>
2020-09-06 01:52:23 -0400

    router to /log

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import AdvRecordLog from "AdvRecordLog";
 import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { ToastProvider } from "react-toast-notifications";
+import AdvRecordLog from "AdvRecordLog";
+
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<ToastProvider autoDismiss autoDismissTimeout="3000">
+    <React.StrictMode>
+		<Router>
+			<Switch>
+				<Route path="/log">
+					<Log />
+				</Route>
+				<Route path="/">
+					<Log />
+				</Route>
+			</Switch>
+		</Router>
+	</React.StrictMode>,
+    document.getElementById("root")
+);
+
+// function Home() {
+// 	// TO BE DONE
+// }
+
+function Log() {
+    return (
+        <ToastProvider autoDismiss autoDismissTimeout="3000">
 			<AdvRecordLog />
 		</ToastProvider>
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+    );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
