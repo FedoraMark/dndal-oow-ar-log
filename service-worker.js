5ea76cf0ab901593c809f274613254dd0aca3fcb
FedoraMark <fedora.mark@icloud.com>
2020-09-06 23:35:54 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c8b2ae19d940a6650389fdf384a85d0.js"
+  "/precache-manifest.8245eaa41482dcc28a9ebe793d2f96f4.js"
 );
 
 self.addEventListener('message', (event) => {
